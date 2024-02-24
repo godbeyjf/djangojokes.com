ac42bed59b510d1dad6841567caf3c123dbff44f
godbeyjf <139922886+godbeyjf@users.noreply.github.com>
2024-02-24 11:40:52 -0600

    migrate custom user model

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -41,8 +41,10 @@ INSTALLED_APPS = [
 
     # local apps
 
+    'common.apps.CommonConfig',
     'jokes.apps.JokesConfig',
     'pages.apps.PagesConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -81,15 +83,17 @@ WSGI_APPLICATION = 'djangojokes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-         'USER': 'postgres',
-        'PASSWORD': 'friday',
+          'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jokes',
+        'USER': 'postgres',
+        'PASSWORD': '9393',
         'HOST': 'localhost',
         'PORT': 5432
     }
 }
 
+# AUTHENTICATION SETTINGS
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -108,7 +112,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
