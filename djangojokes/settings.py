c6319c6cb99d6d0eee21a903718449c297426b7d
godbeyjf <139922886+godbeyjf@users.noreply.github.com>
2024-02-24 16:52:35 -0600

    creating job app

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     # local apps
 
     'common.apps.CommonConfig',
+    'jobs.apps.JobsConfig',
     'jokes.apps.JokesConfig',
     'pages.apps.PagesConfig',
     'users.apps.UsersConfig',
