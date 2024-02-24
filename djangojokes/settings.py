075fe59b1e5d8c5b4c86f6188970424eb6e0810e
godbeyjf <139922886+godbeyjf@users.noreply.github.com>
2024-02-24 17:15:38 -0600

    crispy forms

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third-party
+    'crispy_forms',
+    'crispy_bootstrap5',
+
     # local apps
 
     'common.apps.CommonConfig',
@@ -48,6 +52,10 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'users.apps.UsersConfig',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
